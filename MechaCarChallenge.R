6176a7d17a6746ad86d08a86f8cf831e1a71d823
Amandah  Antony <amandahantony@Amandahs-MacBook-Air.local>
2021-04-11 21:16:34 -0400

    Adding challenge

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -7,3 +7,9 @@ mechacar_mpg <- read.csv('MechaCar_mpg.csv',check.names = F,stringsAsFactors = F
 #Perform linear regression 
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mechacar_mpg)
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mechacar_mpg))
+
+#Deliverable 2: Creating Visualizations for Trip Analysis
+suspension <- read.csv('Suspension_Coil.csv',check.names = F,stringsAsFactors = F)
+
+total_summary <- suspension %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+lot_summary <- suspension %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
